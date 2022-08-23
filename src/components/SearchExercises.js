93cfadf6639c1d8d43dbfaee7c437efe91531c46
Pankaj Desai <desaipankaj740@gmail.com>
2022-08-23 20:01:31 +0530

    bodyPart(Card)

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -6,10 +6,9 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises,bodyPart,setBodyPart}) => {
 
     const [search, setSearch] =  useState(''); 
-    const [exercises,setExercises] = useState([]);
     const [bodyParts, setBodyParts] = useState([]);
 
     useEffect(() => {
@@ -88,7 +87,9 @@ const SearchExercises = () => {
       </Box>
 
       <Box sx={{position:'relative',width:'100%',p:'20px'}}>
-        <HorizontalScrollbar data={bodyParts}/>
+        <HorizontalScrollbar data={bodyParts}
+        bodyPart={bodyPart} setBodyPart={setBodyPart}
+        />
         
       </Box>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,23 @@ import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([]);
   return (
-     <Box>
-      <HeroBanner/>
-      <SearchExercises/>
-      <Exercises/>
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
 
-     </Box>
+    </Box>
   )
 }
 
