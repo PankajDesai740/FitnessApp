6157c5507bd859081a99b8d8fdc5a431597f36e0
Pankaj Desai <desaipankaj740@gmail.com>
2022-08-29 22:02:47 +0530

    ExerciseVideos done

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -6,7 +6,7 @@ import { Box, Stack, Typography } from '@mui/material';
 const ExerciseVideos = ({exerciseVideos, name}) => {
   return (
     <Box  sx={{marginTop:{lg :'200px', xs:'20px'}}} p="20px">
-      <Typography variant="h5" mb="33px">
+      <Typography variant="h4" mb="33px">
         Watch <span style ={{color:"#ff2625", textTransform:"capitalize"}}>{name}</span> exercise videos
       </Typography>
       <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center"
@@ -23,6 +23,14 @@ const ExerciseVideos = ({exerciseVideos, name}) => {
               rel="noreferrer"
               >
                <img src={item.video.thumbnails[0].url} alt={item.video.title} /> 
+               <Box>
+                <Typography variant="h6" color='#000'>
+                  {item.video.title}
+                </Typography>
+                <Typography variant="h8" color='#000'>
+                  {item.video.channelName}
+                </Typography>
+               </Box>
             </a>
           ))}
       </Stack>
