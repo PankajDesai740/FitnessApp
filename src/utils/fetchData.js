22d63257b88bd95f78fa36e95df3751986ddeeda
Pankaj Desai <desaipankaj740@gmail.com>
2022-09-02 17:13:12 +0530

    Youtb api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -12,7 +12,7 @@ export const exerciseOptions =  {
   export const youtubeOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '840cb5d81dmsh4eea2a945af18e6p1510a0jsn407e476282c5',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY1,
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
